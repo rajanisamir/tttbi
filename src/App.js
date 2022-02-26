58c72e24d294801750471335a3ddf168187916b4
rajanisamir <rajanisamir@gmail.com>
2022-02-26 15:09:47 -0600

    Added button icons and transparent menus; added menu opening logic

--- src/App.js
+++ src/App.js
@@ -3,38 +3,55 @@ import "./App.css";
 import TaskManager from "./components/TaskManager";
 import Profile from "./components/Profile";
 import Map from "./components/Map";
+import { MdOutlineMap } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineAssignment } from "react-icons/md";
 
 function App() {
-  const [profile, p_visible] = useState(false);
-  const [assignments, a_visible] = useState(false);
-  const [map, m_visible] = useState(false);
+  const [currentMenu, setCurrentMenu] = useState("None");
 
   return (
-    <div className="main-view">
-      <button className="button" onClick={() => p_visible(!profile)}>
-        profile
-      </button>
-      <button className="button" onClick={() => a_visible(!assignments)}>
-        assignments
-      </button>
-      <button className="button" onClick={() => m_visible(!map)}>
-        map
-      </button>
-      {assignments && (
-        <div className="task-manager-tab">
-          <TaskManager />
-        </div>
-      )}
-      {profile && (
-        <div className="profile-tab">
-          <Profile />
-        </div>
-      )}
-      {map && (
-        <div className="map-tab">
-          <Map />
-        </div>
-      )}
+    <div>
+      <div className="main-view">
+        {currentMenu === "Tasks" && (
+          <div className="task-manager-tab">
+            <TaskManager />
+          </div>
+        )}
+        {currentMenu === "Profile" && (
+          <div className="profile-tab">
+            <Profile />
+          </div>
+        )}
+        {currentMenu === "Map" && (
+          <div className="map-tab">
+            <Map />
+          </div>
+        )}
+      </div>
+      <div>
+        <MdOutlineMap
+          onClick={() => {
+            setCurrentMenu(currentMenu === "Map" ? "None" : "Map");
+          }}
+          className="menu-button"
+          id="map-button"
+        />
+        <CgProfile
+          onClick={() => {
+            setCurrentMenu(currentMenu === "Profile" ? "None" : "Profile");
+          }}
+          className="menu-button"
+          id="profile-button"
+        />
+        <MdOutlineAssignment
+          onClick={() => {
+            setCurrentMenu(currentMenu === "Tasks" ? "None" : "Tasks");
+          }}
+          className="menu-button"
+          id="tasks-button"
+        />
+      </div>
     </div>
   );
 }
